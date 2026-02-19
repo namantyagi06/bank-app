306c03f209e578cb4ebc90ec4c2d2e81a1cec8f1
namantyagi06 <tyaginaman070@gmail.com>
2026-02-19 16:31:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Online Bank Mini System
+
+Features:
+- Create Account
+- Transfer Money
+- KYC Validation
+- Balance Check
+
+Tech:
+Python Flask
+
+GitHub: <repo link>
+Live URL: <render link>
+
---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+accounts = {}
+
+class Account:
+    def __init__(self, acc, name, balance, kyc):
+        self.accountNo = acc
+        self.holderName = name
+        self.balance = balance
+        self.kycVerified = kyc
+
+@app.route("/")
+def home():
+    return render_template("index.html", accounts=accounts)
+
+@app.route("/create", methods=["POST"])
+def create():
+    acc = request.form["acc"]
+    name = request.form["name"]
+    bal = float(request.form["bal"])
+    kyc = request.form.get("kyc")
+
+    accounts[acc] = Account(acc,name,bal,True if kyc else False)
+    return "Account Created"
+
+@app.route("/transfer", methods=["POST"])
+def transfer():
+    sender = request.form["sender"]
+    receiver = request.form["receiver"]
+    amount = float(request.form["amount"])
+
+    if not accounts[sender].kycVerified:
+        return "Sender KYC not verified"
+
+    if accounts[sender].balance < amount:
+        return "Insufficient Balance"
+
+    accounts[sender].balance -= amount
+    accounts[receiver].balance += amount
+
+    return "Transfer Successful"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
